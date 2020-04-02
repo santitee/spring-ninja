512108fccb21ff330bc1f22a7d562a9ce27154a1
santitee <santitee@gmail.com>
2020-04-02 15:08:08 +0700

    first commit

---
+++ src/main/java/com/santitee/springninja/SpringNinjaApplication.java
@@ -0,0 +1,13 @@
+package com.santitee.springninja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNinjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNinjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/santitee/springninja/SpringNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santitee.springninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNinjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
